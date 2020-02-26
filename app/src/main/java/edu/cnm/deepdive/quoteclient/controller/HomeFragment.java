0d65aa4ff83f8940e13803683e64f52c0175fe5c
Jawid Mohammadi <mmohammadi@cnm.edu>
2020-02-26 16:48:16 -0700

    Setup Repository and viewModel

---
+++ app/src/main/java/edu/cnm/deepdive/quoteclient/controller/HomeFragment.java
@@ -0,0 +1,37 @@
+package edu.cnm.deepdive.quoteclient.controller;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
+import edu.cnm.deepdive.quoteclient.R;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel viewModel;
+    private TextView quoteText;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+        ViewGroup container, Bundle savedInstanceState) {
+                View root = inflater.inflate(R.layout.fragment_home, container, false);
+        quoteText = root.findViewById(R.id.quote_text);
+        root.setOnClickListener((view) -> viewModel.refresh());
+        return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        viewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+        viewModel.getQuote().observe(getViewLifecycleOwner(), (quote) ->
+            quoteText.setText(quote.getText()));
+    }
+
+}
