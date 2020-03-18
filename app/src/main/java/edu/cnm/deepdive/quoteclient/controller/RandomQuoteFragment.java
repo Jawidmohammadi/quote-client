2c496a996bc83fce252ab1a3114356ef18300ca7
Jawid Mohammadi <mmohammadi@cnm.edu>
2020-03-18 12:37:23 -0600

    added Quote Fragment and Quote RecyclerView

--- app/src/main/java/edu/cnm/deepdive/quoteclient/controller/RandomQuoteFragment.java
+++ app/src/main/java/edu/cnm/deepdive/quoteclient/controller/RandomQuoteFragment.java
@@ -8,28 +8,26 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
 import edu.cnm.deepdive.quoteclient.R;
 
-public class HomeFragment extends Fragment {
+public class RandomQuoteFragment extends Fragment {
 
-    private HomeViewModel viewModel;
+    private MainViewModel viewModel;
     private TextView quoteText;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
         ViewGroup container, Bundle savedInstanceState) {
                 View root = inflater.inflate(R.layout.fragment_home, container, false);
         quoteText = root.findViewById(R.id.quote_text);
-        root.setOnClickListener((view) -> viewModel.refresh());
+        root.setOnClickListener((view) -> viewModel.refreshRandom());
         return root;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        viewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+        viewModel = new ViewModelProvider(getActivity()).get(MainViewModel.class);
         viewModel.getQuote().observe(getViewLifecycleOwner(), (quote) ->
             quoteText.setText(quote.getText()));
     }
---
+++ app/src/main/java/edu/cnm/deepdive/quoteclient/view/QuoteRecyclerAdapter.java
@@ -0,0 +1,81 @@
+package edu.cnm.deepdive.quoteclient.view;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import edu.cnm.deepdive.quoteclient.model.Quote;
+import edu.cnm.deepdive.quoteclient.view.QuoteRecyclerAdapter.Holder;
+import java.util.List;
+
+public class QuoteRecyclerAdapter extends RecyclerView.Adapter<Holder> {
+
+
+
+  // Necessary Fields for adapting a collection to arecyclerView:
+  // - Context
+  // - Collection
+
+  private final Context context;
+  private final List<Quote> quotes;
+
+  public QuoteRecyclerAdapter(Context context,
+      List<Quote> quotes) {
+    this.context = context;
+    this.quotes = quotes;
+  }
+
+  // Overrides of methods to
+  //  - return the number of items in the collection.
+  //  - return a holder instance for an inflated layout
+  //  - binds a holder instance an object ata a specified position in the recycler view.
+
+
+
+  @NonNull
+  @Override
+  public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    // TODO use a custom layout
+    View root = LayoutInflater.from(context).inflate(android.R.layout.simple_list_item_1, parent, false);
+    return new Holder(root);
+  }
+
+  @Override
+  public void onBindViewHolder(@NonNull Holder holder, int position) {
+    holder.bind(position, quotes.get(position));
+  }
+
+  @Override
+  public int getItemCount() {
+    return quotes.size();
+  }
+
+  // Holder class that
+  // - Holds an inflated layout
+  // - Binds a specified object in the collection to the view objects in the inflated layout.
+  class Holder extends RecyclerView.ViewHolder{
+
+    private final View root;
+    // More fields for view objects inside root
+
+    private Holder(View root) {
+      super(root);
+      this.root = root;
+    // Find view objects by ID inside root.
+    }
+
+    private void bind(int position, Quote quote) {
+      // TODO Set contexts of view fields to fields kjof quote.
+      ((TextView) root).setText(quote.getText());
+      // TODO Set any event Listener.
+    }
+  }
+
+  // Event listener interface declarations.
+}
+
+
+
